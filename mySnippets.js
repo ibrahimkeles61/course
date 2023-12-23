c1b4c9f2b1f2bc45be601784111cdaccafe036b1
İbrahim Keleş <ibrahimkeles.work@gmail.com>
2023-12-23 09:49:38 +0300

    course 0

---
+++ mySnippets.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function _____() {
+	return (
+		<View style={styles.container}>
+			<Text>_____</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {},
+});
